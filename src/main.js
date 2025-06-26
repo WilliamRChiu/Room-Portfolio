ecef85a98805238a2043a74d3a0197435d7172a8
William Chiu <williamrchiu18@gmail.com>
2025-06-26 13:27:46 -0400

    push

--- src/main.js
+++ src/main.js
@@ -1,24 +1,117 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.scss";
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { DRACOLoader } from "three/addons/loaders/DRACOLoader.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const canvas = document.querySelector(".experience-canvas");
 
-setupCounter(document.querySelector('#counter'))
+const sizes = {
+  height: window.innerHeight,
+  width: window.innerWidth,
+};
+
+/*Loaders*/
+const textureLoader = new THREE.TextureLoader();
+
+//Model Loader
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath("/draco/");
+
+const loader = new GLTFLoader();
+loader.setDRACOLoader(dracoLoader);
+
+const textureMap = {
+  First: "/textures/TextureSetOneDay.webp",
+  Second: "/textures/TextureSetTwoDay.webp",
+  Third: "/textures/TextureSetThreeDay.webp",
+  Fourth: "/textures/TextureSetFourDay.webp",
+  Fifth: "/textures/TextureSetFiveDay.webp",
+  Targets: "/textures/TargetTextureSetDay.webp",
+};
+
+const loaderTextures = {
+  day: {},
+};
+
+Object.entries(textureMap).forEach(([key, paths]) => {
+  const dayTexture = textureLoader.load(paths);
+  dayTexture.flipY = false;
+  dayTexture.colorSpace = THREE.SRGBColorSpace;
+  loaderTextures.day[key] = dayTexture;
+});
+
+loader.load("/models/Room_Portfolio_Compressed.glb", (glb) => {
+  const specificKeys = ["First", "Second", "Third", "Fourth", "Fifth"];
+  glb.scene.traverse((child) => {
+    if (child.isMesh) {
+      const matchedKey = specificKeys.find((key) => child.name == key);
+      if (matchedKey) {
+        child.material = new THREE.MeshBasicMaterial({
+          map: loaderTextures.day[matchedKey],
+        });
+      } else {
+        child.material = new THREE.MeshBasicMaterial({
+          map: loaderTextures.day["Targets"],
+        });
+      }
+      if (child.material.map) {
+        child.material.map.minFilter = THREE.LinearFilter; //prevent seams from forming if zoom out
+      }
+    }
+  });
+  scene.add(glb.scene);
+});
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  1000
+);
+camera.position.set(3.9675245476668666, 5.889062656688702, 6.212748218287423);
+
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)); //Note: 2 is max pixel ratio should go for performance
+//note: pixelratio is ratio between physical pixels on a device and the CSS pixels used by the browser.  If i say pixel ratio is 2, then every CSS pixel (which is 1x1) is mapped to a 2x2 physical pixels
+
+/*orbit controls*/
+//controls.update() must be called after any manual changes to the camera's transform
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true; //allows slowing glide after movement of camera
+controls.dampingFactor = 0.05;
+controls.update();
+controls.target.set(
+  -0.20356508065730236,
+  0.47454386084186306,
+  -0.38998272940458945
+);
+/*Event Listeners*/
+
+//Window resize event listener
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  //Update Camera (so scene does not look squished)
+  camera.aspect = sizes.width / sizes.height;
+
+  //Update projection matrix to properly map 3d points to new 2d plane
+  camera.updateProjectionMatrix();
+
+  //Update renderer (so scene renders to correct size canvas and doesn't get blurred, cropped, etc)
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+const render = () => {
+  controls.update();
+
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(render); //this forces window to rerender on every new frame
+};
+
+render();
